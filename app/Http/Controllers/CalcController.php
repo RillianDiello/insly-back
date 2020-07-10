c1d911d1b2071624c2dc133de7c969301e8b2e1a
Rillian Diello <rilliansf@gmail.com>
2020-07-10 16:34:12 -0400

    Validador Erros input

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Response;
 use DateTime;
 
 class CalcController extends Controller
@@ -11,21 +13,37 @@ class CalcController extends Controller
     public function makeCalc(Request $request){
         $params = $request->all();
 
+        $validator = Validator::make(
+            $params,
+            [
+                'valueOfCar' => 'required|numeric|between:100,100000',
+                'taxPercent'     => 'required|numeric|between:0,100',
+                'numInstalments'      => 'required|numeric|between:1,12',
+            ]);
+        $calculate['errors'] = [];
+        if ($validator->fails())
+        {
+            // return response()
+            //     ->json($validator->errors());
+            $calculate['errors'] = $validator->errors();
+            $calculate['colunms'] = [];
+            $calculate['totais'] = [];
+            $calculate['instalments'] = [];
+            return response()->json($calculate);
+        }
         
-
         $valueOfCar = (float) $params['valueOfCar'];
         $taxPercent = (float) $params['taxPercent'];
         $numInstalments = (int) $params['numInstalments'];
 
+      
         $calc = new Calc();
         $calc->setValueOfCar($valueOfCar);
         $calc->setTaxPercent($taxPercent);
         $calc->setNumInstalments($numInstalments);
         $calc->CalculateInsuranceMethod();
 
-        $calculate['colunms'] = $calc->colValues();
-        // $calculate['titles'] = $calc->getTitles();
-    
+        $calculate['colunms'] = $calc->colValues();     
         $calculate['totais'] = $calc->getTotalsPolicy();
         $calculate['instalments'] = $calc->getInstaments();
         
@@ -87,7 +105,8 @@ class Calc
     }
 
     public function getTotalsPolicy(){
-        return [            
+        return [      
+            'policy' => 'Polcy'          ,      
             'base_premium' => number_format($this->calculate->getBasePremium(),2, '.', ''),
             'comission' => number_format($this->calculate->getComission(),2, '.', ''),
             'tax' => number_format($this->calculate->getTax(),2, '.', ''),
